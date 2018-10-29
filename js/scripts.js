e9e50cc601133f4354ba2c9ee7f975bf796d6f5c
Katrina Hockman and Sheila Stephen <sheila@>
2018-10-29 14:33:16 -0700

    make the clickable toggle function on the HTML page

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,36 @@
 function PlaceBeen() {
-  this.placesbeen = [],
-  this.currentId = 0
-}
-
-PlaceBeen.prototype.addPlace = function(place){
-  place.id = this.assignId();
-  this.placesbeen.push(place);
-}
-
-PlaceBeen.prototype.assignId =function() {
-  this.currentId += 1;
-  return this.currentId;
+  this.placesbeen = []
 }
 
 function Place(location, landmark, dates, notes) {
   this.location = location,
   this.landmark = landmark,
   this.dates = dates,
-  this.notes = notes
+  this.notes = notes;
 }
 
-Place.prototype.displayProperties = function() {
-  return ths.location + this.landmark + this.dates + this.notes;
-  //console.log(ths.location + this.landmark + this.dates + this.notes);
+PlaceBeen.prototype.addPlace = function(place) {
+  this.placesbeen.push(place);
+  return this.location + this.landmark + this.dates + this.notes;
 }
-
-var placeBeen = new PlaceBeen();
-var  locationPlace = new Place("PDX", "Bridges", "3-3-2009", "Rained");
-placeBeen.addPlace(locationPlace);
 //User Interface Logic
+var placeList = new PlaceBeen();
 $(document).ready(function() {
-  $("#results").submit(function(event){
+  $("#add-place").submit(function(event){
   event.preventDefault();
   var inputtedCity = $("input#city").val();
   var inputtedLandmark = $("input#landmark").val();
   var inputtedDates = $("input#dates").val();
   var inputtedNotes = $("input#notes").val();
   var newPlace = new Place(inputtedCity, inputtedLandmark, inputtedDates, inputtedNotes);
-  placeBeen.addPlace(newPlace)
-  console.log(placeBeen.placesbeen)
+  placeList.addPlace(newPlace);
 
-  // var locationPlace2 = new Place("Seattle", "Space Needle", "5-11-2012", "Opened for visitors");
+  $("#placeList").append("<li>" + inputtedCity + "</li>");
 
-
-  $("#showResult").text(displayProperties);
-  console.log(PlaceBeen.displayProperties);
+  $("li").click(function(){
+    $("#showresult").slideToggle();
+    //$("#showresult").addClass("light");
+  });
 
 });
-
-//   $("#seattle").submit(function(){
-//   event.preventDefault();
-//   return locationPlace2;
-// });
-
 });
